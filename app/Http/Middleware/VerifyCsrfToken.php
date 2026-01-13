db7b14229cfe67f7722014368290639f6a31781c
John Gaurav <prof.johngaurav@gmail.com>
2026-01-13 12:47:19 +0530

    weebhook added

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'webhook',             // If Easebuzz is posting to your /webhook
+        'easebuzz/webhook',             // If Easebuzz is posting to your /webhook
         'erp/student/payment-success',     // If they POST to your success URL
         'erp/student/payment-failure'      // If they POST to your failure URL
     ];
