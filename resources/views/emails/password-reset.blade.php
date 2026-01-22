20e8b8b7155456b3a6f3e942d60f5b2d8d285d9c
John Gaurav <prof.johngaurav@gmail.com>
2026-01-23 01:42:09 +0530

    fxing pwr

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -50,7 +50,7 @@
                                             password has been generated. To reset your password, click the
                                             following link and follow the instructions.
                                         </p>
-                                        <a href="http://erpsalesiancollege.sdbinc.org/verify-mail-reset-token/{{$details['token']}}"
+                                        <a href="https://erpsalesiancollege.sdbinc.org/verify-mail-reset-token/{{$details['token']}}"
                                             style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;"
                                             target="_blank">Click Here </a>
                                     </td>
