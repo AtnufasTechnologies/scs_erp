b2445780f66f23d79159e39897b82bde3473b39a
John Gaurav <prof.johngaurav@gmail.com>
2026-05-15 14:26:53 +0530

    done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -39,14 +39,14 @@ class LoginController extends Controller
                         //check if dept admin has assigned department
                         $deptAdminDept = User::with('subjectdeptadmin.subject')->where('id', $user->id)->first();
                         if ($deptAdminDept->subjectdeptadmin) {
-                            //  return redirect()->route('department.dashboard')->with('success', 'Login Success');
-                            Auth::logout();
-                            return redirect('/')->with('info', 'Development in Progress...Please Wait till Product Completion');
+                            return redirect()->route('department.dashboard')->with('success', 'Login Success');
+                            // Auth::logout();
+                            //return redirect('/')->with('info', 'Development in Progress...Please Wait till Product Completion');
                         } else {
 
                             Auth::logout();
-                            return redirect('/')->with('info', 'Development in Progress...Please Wait till Product Completion');
-                            // return redirect('/')->with('error', 'No Department Assigned. Please contact Admin');
+                            //    return redirect('/')->with('info', 'Development in Progress...Please Wait till Product Completion');
+                            return redirect('/')->with('error', 'No Department Assigned. Please contact Admin');
                         }
 
                     case 'account-office-incharge':
